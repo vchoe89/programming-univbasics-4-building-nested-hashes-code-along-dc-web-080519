7fa5a4fb1a08a42e1d482356ca8a7bf5f13e37e6
Victor Jian Choe <vchoe89@gmail.com>
2019-07-25 00:53:59 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,14 +1,19 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch
-      :matriarch
-      :hero
-      :hero_friends
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => [],
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => [],
+      }
+    
+  }
 
   
 end
