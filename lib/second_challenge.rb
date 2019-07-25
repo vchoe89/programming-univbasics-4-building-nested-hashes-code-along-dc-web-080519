b67799142cde4d4ef57da4a9bd8e1e90ed91b3c1
Victor Jian Choe <vchoe89@gmail.com>
2019-07-25 01:10:43 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,19 +7,9 @@ def second_challenge
       :hero_friends => [],
       }, 
     :capulet => {
-      :patriarch => {
-        name: "Lord Capulet",
-        age: "50"
-      },
-      :matriarch => {
-        name: "Lady Capulet",
-        age: "51"
-      },
-      :heroine => {
-        name: "Juliet",
-        age: "15",
-        status: "alive"
-      },
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [],
       }
     
