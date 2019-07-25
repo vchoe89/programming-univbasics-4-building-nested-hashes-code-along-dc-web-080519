b4ef23e35e72cdfc0a10e5f3a9658ed3cc864f51
Victor Jian Choe <vchoe89@gmail.com>
2019-07-25 01:09:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => { name: "Lord Montague", age: "53"},
-      :matriarch => { name: "Lady Montague",
-        age: "54"
-      },
-      :hero => {
-        name: "Romeo",
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo",
         age: "15",
         status: "alive"
       },
