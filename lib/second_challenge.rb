a43e89d2048233be5d6e95517ea40fdab0674856
Victor Jian Choe <vchoe89@gmail.com>
2019-07-25 00:50:57 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch
+      :matriarch
+      :hero
       }, 
     :capulet => {
       #your key/value pairs here!
