fde3743758df37efd41f3a9e37807d5c7552644e
Victor Jian Choe <vchoe89@gmail.com>
2019-07-25 01:38:26 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,6 +22,8 @@ def bonus
 
   #code your solution here:
 
+montague[:status] = dead
+capulet[:status] = dead
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
