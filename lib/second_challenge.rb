ff1eec64cca5a96c76c201f2d2b497ef7d158f74
Victor Jian Choe <vchoe89@gmail.com>
2019-07-25 01:14:29 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -11,9 +11,6 @@ def second_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [],
-      }
-    
+    }
   }
-
-  
 end
